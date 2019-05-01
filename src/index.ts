e695b546f0429029112f414c2dd90826dd1a06b9
Joe Haddad <timer150@gmail.com>
2019-04-30 21:04:00 -0400

    Write first version of unbrotli

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+import peek from 'peek-stream';
+import { Duplex } from 'stream';
+import through from 'through2';
+import zlib, { BrotliOptions } from 'zlib';
+
+// https://stackoverflow.com/a/39032023/4397028
+function isBrotli(buf: Buffer | string) {
+  if (!buf || buf.length < 4) {
+    return false;
   }
-  return a + b;
-};
+  return (
+    buf[0] === 0xce && buf[1] === 0xb2 && buf[2] === 0xcf && buf[3] === 0x81
+  );
+}
+
+export function unbrotli(options?: BrotliOptions): Duplex {
+  return peek({ newline: false, maxBuffer: 10 }, function(
+    data: Buffer | string,
+    swap: (err: Error | undefined, parser: Duplex) => void
+  ) {
+    if (isBrotli(data)) {
+      return swap(undefined, zlib.createBrotliDecompress(options));
+    }
+
+    return swap(undefined, through());
+  });
+}
---
+++ test/unbrotli.test.ts
@@ -0,0 +1,10 @@
+import { unbrotli } from '../src';
+
+describe('unbrotli', () => {
+  it('does nothing', () => {
+    expect(unbrotli);
+  });
+  it('decompresses', () => {
+    expect(unbrotli);
+  });
+});
